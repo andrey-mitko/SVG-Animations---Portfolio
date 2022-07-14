06e3dc03ccd5f9a25ee55849014cf0f40c97bd7a
Andrey Mitko <andrey@crowdhaus.co.uk>
2022-07-14 17:36:15 +0100

    Added animated blobs

---
+++ jessie.js
@@ -0,0 +1,31 @@
+const headerTag = document.querySelector('header');
+const blobGroups = document.querySelectorAll('g.blob');
+const sectionTags = document.querySelectorAll('section');
+
+const easing = (x) => {
+    return x * x * x;
+}
+
+const fadeHeader = () => {
+    const pixels = window.pageYOffset;
+    headerTag.style.opacity = 1 - easing(pixels / 500);
+}
+
+const checkBlobs = () => {
+    const pixels = window.pageYOffset;
+    blobGroups.forEach((blob, index) => {
+
+        const currentSection = sectionTags[index];
+
+        if (pixels > currentSection.offsetTop - 300) {
+            blob.classList.add('in-view');
+        } else {
+            blob.classList.remove('in-view');
+        }
+    });
+}
+
+window.addEventListener('scroll', () => {
+    fadeHeader();
+    checkBlobs();
+});
